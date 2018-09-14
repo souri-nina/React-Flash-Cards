9a2be630449513916a506d78bbd5031d126e6122
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-09-14 01:22:57 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Flash Cards</h1>
+        </header>
+        <p className="App-intro">
+          Yay - React
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
